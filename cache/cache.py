ebbd737173267fcba78dde3bf045dd111306531b
John <johnmlowery@gmail.com>
2024-05-23 20:43:37 -0500

    something

--- cache/cache.py
+++ cache/cache.py
@@ -2,14 +2,12 @@ from redis.asyncio import Redis
 from contextlib import asynccontextmanager
 import os
 
-REDIS_HOST = os.getenv("REDIS_HOST")
-REDIS_PORT = os.getenv("REDIS_PORT")
-REDIS_DB = os.getenv("REDIS_DB")
+REDIS_URL = os.getenv("REDIS_URL")
 
 
 @asynccontextmanager
 async def redis_connection():
-    client = await Redis(host=REDIS_HOST, port=REDIS_PORT, db=0, decode_responses=True)
+    client = await Redis.from_url(REDIS_URL, decode_responses=True)
     try:
         yield client
     finally:
--- database.py
+++ database.py
@@ -2,15 +2,7 @@ from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
 from sqlalchemy.orm import sessionmaker
 import os
 
-POSTGRES_USER = os.getenv("POSTGRES_USER")
-POSTGRES_PASSWORD = os.getenv("POSTGRES_PASSWORD")
-POSTGRES_PORT = os.getenv("POSTGRES_PORT")
-POSTGRES_HOST = os.getenv("POSTGRES_HOST")
-POSTGRES_DB = os.getenv("POSTGRES_DB")
-
-POSTGRES_URI = f"postgresql+asyncpg://{POSTGRES_USER}:{POSTGRES_PASSWORD}@{POSTGRES_HOST}:{POSTGRES_PORT}/{POSTGRES_DB}"
-
-engine = create_async_engine(POSTGRES_URI)
+engine = create_async_engine(os.getenv("DATABASE_URL"))
 
 
 async_session_factory = sessionmaker(engine, class_=AsyncSession, expire_on_commit=False)
