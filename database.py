451f5f1096c6229bc78447fab3448fb528da61eb
John <johnmlowery@gmail.com>
2024-05-26 17:35:56 -0500

    docker stuff

--- database.py
+++ database.py
@@ -2,6 +2,8 @@ from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
 from sqlalchemy.orm import sessionmaker
 import os
 
+print(os.getenv("DATABASE_URL"))
+
 engine = create_async_engine(os.getenv("DATABASE_URL"))
 
 
